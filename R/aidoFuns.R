8f77f061a863afe06c77faed4b36f456d110a2dc
Duncan Temple Lang <duncan@r-project.org>
2017-09-18 19:22:04 -0700

    improved some of the functions

--- R/aidoFuns.R
+++ R/aidoFuns.R
@@ -1,15 +1,16 @@
 #
 # Todo
-#  allow the caller of getOutbreaks to specify the disease by name and we map it an id.
-#  get the diseases locally so we don't have to an online request.
+#  allow the caller of getOutbreaks to specify the disease by name and we map it to an id.
+#  When we get the outbreaks as a data frame, then go and fill the location information on the unique location ids.
 #
 #  Collapse the result of the admin_level (getLocationAdmin) into a data frame.
-#  [done]check the admin_level - add process $"next" for pages
+#
 #  vectorize getDisease() and getLocation()
+#
+#  [done]check the admin_level - add process $"next" for pages
 #  [done] in getLocation() allow the caller to give us the URL from the result rather than location id.
 #  [done] convert the times to Date (or POSIXct)
-
-#  When we get the outbreaks as a data frame, then go and fill the location information on the unique location ids.
+#  [done] get the diseases locally so we don't have to perform an online request.
 #
 #
 getOutbreaks =
@@ -31,8 +32,11 @@ function(disease, location = character(), max = Inf, url = "http://aido.bsvgatew
         tmp = fromJSON(getURLContent(tmp$"next", curl = curl))
         ans = c(ans, tmp$results)
     }
-    
-    convertFun(ans)
+
+    if(is.null(convertFun))
+       ans
+    else
+       convertFun(ans)
 }
 
 if(FALSE) {
@@ -78,14 +82,14 @@ function(location = character(), admin = integer(), max = Inf,
          curl = getCurlHandle(..., followlocation = TRUE), ...)
 {
     if(!missing(admin))
-        return(getLocationAdmin(admin, url = url, curl = curl))
+        return(getLocationAdmin(admin, max = max, url = url, curl = curl))
         #args = list(admin_level = admin)
     else if(grepl("^http.*/[0-9]+/?$", location))
+            # if the location contains the location identifier, then this is the API URL to query directly.        
         return(fromJSON(getURLContent(location, curl = curl)))
     else if(!grepl("^[0-9]+$", location))
         args = list(search = location)
     else {
-# if the location is the URL.
         url = paste0(url, location)
         return(fromJSON(getURLContent(url, curl = curl)))
     }
@@ -109,6 +113,9 @@ function(level, max = Inf, url = "http://aido.bsvgateway.org/api/locations/",
 }
 
 getDiseases =
+    #
+    # get all the disease name and id pairs in the database
+    #
 function(max = Inf, url = "http://aido.bsvgateway.org/api/diseases",
          curl = getCurlHandle(..., followlocation = TRUE), ...)
 {
@@ -118,22 +125,33 @@ function(max = Inf, url = "http://aido.bsvgateway.org/api/diseases",
         tmp = fromJSON(getURLContent(tmp$"next", curl = curl))
         ans = c(ans, tmp$results)
     }
-    convertDisease(ans)
+    convertDisease(ans, dataFrame = FALSE)
 }
 
 convertDisease =  convert2DataFrame =
-function(d, vars = c("id", "name"))
+function(d, vars = c("id", "name"), dataFrame = TRUE)
 {
-    ans = as.data.frame(lapply(vars, function(x) sapply(d, `[[`, x)), stringsAsFactors = FALSE)
-    names(ans) = vars
+   tmp = lapply(vars, function(x) sapply(d, `[[`, x))    
+   if(dataFrame) {
+        ans = as.data.frame(tmp, stringsAsFactors = FALSE)
+        names(ans) = vars
+    } else {
+        ans = structure(tmp[[1]], names = tmp[[2]])
+    }
     ans
 }
 
 
 getDiseaseID =
 function(name, local = TRUE,
-         diseases = if(local) getDiseaseDB() else getDiseases(curl = curl),
+         diseases = if(local) diseaseMap else getDiseases(curl = curl),
          curl = getCurlHandle(..., followlocation = TRUE), ...)
 {
+    i = pmatch(tolower(name), tolower(names(diseases)))
+    if(all(is.na(i)))
+        stop("No matching disease(s): ", paste(name, collapse = ", "))
+    else if(any(is.na(i)))
+        warning("No matching disease: ", name[is.na(i)])
 
+    diseases[i[!is.na(i)]]
 }
